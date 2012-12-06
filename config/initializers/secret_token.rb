d9da522551e4c2f81360e45dac2da0863ec2d14b
ZeroLinux <maganazero5@gmail.com>
2012-12-05 22:38:36 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zerolinux5::Application.config.secret_token = 'ae4731bc826a56c806aa309ceada000737424950d53f032faadbd76b9a805464ea535304de7bbe2d530caa3855f20e534aa5b69b8008e41936fee676ff2b0958'

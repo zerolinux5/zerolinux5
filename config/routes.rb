57c32ff3cb4f111cdb02bd9984fb14d04fc87bf9
ZeroLinux <maganazero5@gmail.com>
2012-12-08 13:45:01 -0800

    center the title

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Zerolinux5::Application.routes.draw do
+  get "home/index"
+
+  get "home/about"
+
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
